44752c832d6192c6523a7d0c6ff555bf5492bf92
krakowskimateusz <mateuszkrakowski2908@gmail.com>
2024-12-10 16:00:28 +0100

    connection_to_database

---
+++ ExplodeIT/src/DbConfig.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConfig {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/explodeit";
+    private static final String USER = "root"; // Default XAMPP username
+    private static final String PASSWORD = ""; // Default XAMPP password is empty
+
+    public static Connection connect() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to the database!");
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- ExplodeIT/src/Main.java
+++ ExplodeIT/src/Main.java
@@ -1,8 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Button fetchDataButton = new Button("Fetch Data");
+
+        fetchDataButton.setOnAction(event -> {
+            try (Connection connection = DbConfig.connect()) {
+                if (connection != null) {
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery("SELECT * FROM item");
+
+                    while (resultSet.next()) {
+                        System.out.println("Data: " + resultSet.getString("ItemID"));
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        VBox root = new VBox(fetchDataButton);
+        Scene scene = new Scene(root, 300, 200);
+
+        stage.setScene(scene);
+        stage.setTitle("JavaFX and MySQL Demo");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
